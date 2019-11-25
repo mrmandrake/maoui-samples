4d3b475735045897d0e771d133161a879a3c00e9
mr mandrake <manuel.mazzetto@tesigroup.tech>
2019-11-25 01:17:50 +0100

    cleaning buttonxaml

--- ButtonXaml/mono-config.js
+++
@@ -1,6 +0,0 @@
-config = {
- 	vfs_prefix: "managed",
- 	deploy_prefix: "managed",
- 	enable_debugging: 0,
- 	file_list: [ "WebAssembly.Bindings.dll","netstandard.dll","mscorlib.dll","System.dll","Mono.Security.dll","System.Xml.dll","System.Numerics.dll","System.Core.dll","System.Data.dll","System.Transactions.dll","System.Data.DataSetExtensions.dll","System.Drawing.Common.dll","System.IO.Compression.dll","System.IO.Compression.FileSystem.dll","System.ComponentModel.Composition.dll","System.Net.Http.dll","WebAssembly.Net.Http.dll","System.Memory.dll","System.Runtime.Serialization.dll","System.ServiceModel.Internals.dll","System.Xml.Linq.dll","WebAssembly.Net.WebSockets.dll" ],
-}
